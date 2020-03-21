e1fc67309515b2d5d65c6a22974a9abe018a1490
Yanis Vieilly <yanis.vieilly+github@protonmail.com>
2020-03-21 19:02:55 +0100

    Clean up code

--- scripts/custom-order-by-filter.js
+++ scripts/custom-order-by-filter.js
@@ -1,22 +1,20 @@
 angular.module("countriesApp").filter("customOrderBy", [
   "orderByFilter",
-  orderBy => {
-    return function(countries, sortProperty, sortOrderAsc) {
-      if (["continentName", "countryName"].includes(sortProperty)) {
-        return orderBy(countries, sortProperty, !sortOrderAsc);
+  orderBy => (countries, sortProperty, sortOrderAsc) => {
+    if (["continentName", "countryName"].includes(sortProperty)) {
+      return orderBy(countries, sortProperty, !sortOrderAsc);
+    }
+
+    return countries.sort((a, b) => {
+      if (parseFloat(a[sortProperty]) > parseFloat(b[sortProperty])) {
+        return sortOrderAsc ? -1 : 1;
       }
 
-      return countries.sort((a, b) => {
-        if (parseFloat(a[sortProperty]) > parseFloat(b[sortProperty])) {
-          return sortOrderAsc ? -1 : 1;
-        }
+      if (parseFloat(a[sortProperty]) < parseFloat(b[sortProperty])) {
+        return sortOrderAsc ? 1 : -1;
+      }
 
-        if (parseFloat(a[sortProperty]) < parseFloat(b[sortProperty])) {
-          return sortOrderAsc ? 1 : -1;
-        }
-
-        return 0;
-      });
-    };
+      return 0;
+    });
   }
 ]);
